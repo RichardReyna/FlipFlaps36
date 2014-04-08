a159f3ce9952486c52252c513e074b26a1b16cf0
ThroatOfWinter57 <RoniloRagodos57@hotmail.com>
2014-04-08 12:51:12 -0500

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,26 @@
+#include "Simple_window.h"
+#include "Screen2.h"
+//#include "GUI.h"
+//#include "Graph.h"
+
+int main()
+{
+        try
+        {
+                Screen2 win(Point(100, 100), 640, 480, "wind");
+                return gui_main();
+        }
+
+        catch(exception& e)
+        {
+                cerr << "" << e.what() << endl;
+                return 1;
+        }
+
+        catch(...)
+        {
+                cerr << "" << endl;
+                return 2;
+        }
+}
+
