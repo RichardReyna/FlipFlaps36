5853d9f35b10ca822e9e074942556dc3e3a95520
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-11 16:23:58 -0500

    Update Pancake_stack.h

--- Pancake_stack.h
+++ Pancake_stack.h
@@ -4,6 +4,11 @@
 #include "std_lib_facilities_4.h"
 #include "Graph.h"
 #include "Simple_window.h"
+#include <iostream>     // std::cout
+#include <algorithm>    // std::random_shuffle
+#include <vector>       // std::vector
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
 
 class Pancake;
 
@@ -14,12 +19,15 @@ class Pancake_stack
 //              void push_back(Pancake& p);
                 void flip(int);
                 int myrandom(int);
-                vector<int> random_pancake(int);
+                vector <int> random_pancake(int);
                 int calc_score();
+                int find_solution();
         private:
                 // Functions
         private:
-                // Vars
+                int difficulty;
+                int num_flips;
+
 };
 
 #endif
