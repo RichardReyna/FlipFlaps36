74e24af6d5f010d974de2ad0472374bd59ccd308
group-project <madeleinebayeh@gmail.com>
2014-04-11 15:02:08 -0500

    Update Pancake.cpp

--- Pancake.cpp
+++ Pancake.cpp
@@ -38,26 +38,25 @@ void Pancake::flip()
 }
 
 // Randomizes stack order
-void Pancake::stack(vector<Pancake> stack_init, vector<Pancake> stack_rand)
-{/*
-	int size = stack_init.size();
-	int n = size;
-	while (n > 0)
-	{
-		int pos = rand() % (size + 1);
-		if (stack_rand[pos] == NULL)
-		{
-			stack_rand[pos] = stack_init[n];
-			--n;
-		}
-
-		else
-		{
-			continue();
-		}
-	}*/
+int myrandom (int i) { return rand()%i;}
+vector <int> random_pancake (int difficulty) {
+	srand ( unsigned (time(0) ) );
+	vector<int> pancake;
+	for (int i=0; i<difficulty; ++i) 
+		pancake.push_back(i); 
+	random_shuffle ( pancake.begin(), pancake.end(), myrandom);
+	//cout << "pancake contains:";
+	//for (vector<int>::iterator it=pancake.begin(); it!=pancake.end(); ++it)
+	//	cout << ' ' << *it;
+	//	cout << '\n';
 }
-
+//Calculates user's score
+int calc_score(){
+	int score;
+	if (num_flips-find_solution().size() >= 10)
+		score = 0;
+	else score = (100-10*(flips-find_solution().size()))*difficulty;
+}
 void Pancake::find_solution()
 {
 
