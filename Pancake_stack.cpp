ad849ce0518dfd3077f4b33d755f95e51d3c9826
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-11 16:24:51 -0500

    Update Pancake_stack.cpp
    uncommenting random_pancake() and calc_score() break the program

--- Pancake_stack.cpp
+++ Pancake_stack.cpp
@@ -5,23 +5,30 @@ void Pancake_stack::flip(int n)
         //...
 }
 
-// Randomizes stack order
-int myrandom (int i) { return rand()%i;}
-vector <int> random_pancake (int difficulty) {
-	srand ( unsigned (time(0) ) );
-	vector<int> pancake;
-	for (int i=0; i<difficulty; ++i) 
-		pancake.push_back(i); 
-	random_shuffle ( pancake.begin(), pancake.end(), myrandom);
-	//cout << "pancake contains:";
-	//for (vector<int>::iterator it=pancake.begin(); it!=pancake.end(); ++it)
-	//	cout << ' ' << *it;
-	//	cout << '\n';
+int Pancake_stack::myrandom (int i)
+{
+        return rand()%i;
 }
+
+int Pancake_stack::find_solution()
+{
+        //...
+}
+
+vector <int> Pancake_stack::random_pancake (int difficulty)
+{
+//      srand ( unsigned (time(0) ) );
+//      vector<int> pancake;
+//      for (int i=0; i<difficulty; ++i)
+//              pancake.push_back(i);
+//      random_shuffle ( pancake.begin(), pancake.end(), myrandom);
+
+}
+
 //Calculates user's score
-int calc_score(){
-	int score;
-	if (num_flips-find_solution().size() >= 10)
-		score = 0;
-	else score = (100-10*(flips-find_solution().size()))*difficulty;
+int Pancake_stack::calc_score(){
+//      int score;
+//      if (num_flips-find_solution().size() >= 10)
+//              score = 0;
+//      else score = (100-10*(num_flips-find_solution().size()))*difficulty;
 }
