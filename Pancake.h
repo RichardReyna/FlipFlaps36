56638ed1ed39d8e361f06a68f1c8afaa1fc91af5
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-11 15:28:56 -0500

    Update Pancake.h
    changed as per dr. Daugherty's suggestion on piazza

--- Pancake.h
+++ Pancake.h
@@ -2,26 +2,28 @@
 #define PANCAKE_H
 
 #include "std_lib_facilities_4.h"
+#include "Simple_window.h"
+#include "Graph.h"
 
-class Pancake
+class Pancake : public Rectangle
 {
-	public:
-		Pancake(int difficulty_level);
-		int get_difficulty();
-		void set_difficulty(int diff);
-	private:
-		// Functions
-		void init_stack(vector<Pancake> stack_init);
-		void flip();
-		void stack(vector<Pancake> stack_init, vector<Pancake> stack_rand);
-		void find_solution();
-	private:
-		// Vars
-//		vector<Pancake> pancakes;
-//		vector<Pancake> stack;
+        public:
+                Pancake(Point center, int size);
+                //int get_difficulty();
+                //void set_difficulty(int diff);
+        private:
+                // Functions
+                //void init_stack(vector<Pancake> stack_init);
+                //void flip();
+                //void stack(vector<Pancake> stack_init, vector<Pancake> stack_rand);
+                //void find_solution();
+        private:
+                // Vars
+//              vector<Pancake> pancakes;
+//              vector<Pancake> stack;
 
-		int difficulty;
-		int width;
+                //int difficulty;
+                //int width;
 };
 
 #endif
