813d0ffd5e4982e05a6e6163ed4e15e80fad9dac
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-11 09:55:07 -0500

    Update Screen2.cpp
    Added comments to functions

--- Screen2.cpp
+++ Screen2.cpp
@@ -3,6 +3,7 @@
 
 Game_State state = Game_State::Start;
 
+// Construct window, attach/hide buttons
 Screen2::Screen2(Point xy, int w, int h, const string& title)
         : Window(xy, w, h, title),
         to_game_state(Point(x_max() - 200, 0), 70, 20, "TO GAME", cb_to_game_state),
@@ -14,16 +15,21 @@ Screen2::Screen2(Point xy, int w, int h, const string& title)
         to_end_state.hide();
 }
 
+// Change game state to arg val
 void Screen2::change(Game_State s)
 {
         state = s;
 }
 
+// Occurs on click of to_game_state button
+// and changes game state to Game
 void Screen2::game_state_pressed()
 {
         change(Game_State::Game);
 }
 
+// Occurs on click of to_end_state button
+// and changes game state to End
 void Screen2::end_state_pressed()
 {
         change(Game_State::End);
@@ -31,10 +37,10 @@ void Screen2::end_state_pressed()
 
 void Screen2::cb_to_game_state(Address, Address)
 {
-
+        reference_to<Screen2>(pw).game_state_pressed();
 }
 
 void Screen2::cb_to_end_state(Address, Address)
 {
-
+        reference_to<Screen2>(pw).end_state_pressed();
 }
