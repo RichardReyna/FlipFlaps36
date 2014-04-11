ee58a2f8d942b9ca53b9481a844ef1e426909dee
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-11 15:35:37 -0500

    Update Screen2.h

--- Screen2.h
+++ Screen2.h
@@ -7,6 +7,7 @@
 #include "GUI.h"
 #include "Window.h"
 #include "Point.h"
+#include "std_lib_facilities_4.h"
 
 using namespace Graph_lib;
 using namespace std;
@@ -20,15 +21,19 @@ struct Screen2 : Graph_lib::Window
 {
         public:
                 Screen2(Point, int, int, const string&);
+                Game_State get_state();
         private:
                 void change(Game_State s);
+
                 void game_state_pressed();
                 void end_state_pressed();
+
                 static void cb_to_game_state(Address, Address);
                 static void cb_to_end_state(Address, Address);
         private:
                Button to_game_state;
                Button to_end_state;
+               Vector_ref<Shape> r_buffer;
 //               In_box difficulty;
 };
 
