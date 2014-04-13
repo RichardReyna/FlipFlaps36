a7afa1679a4a46d5dd2241f7c7fbc021aaa66ab7
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-13 13:30:14 -0500

    Update Screen2.cpp

--- Screen2.cpp
+++ Screen2.cpp
@@ -2,7 +2,11 @@
 #include "Graph.h"
 #include "Screen2.h"
 
+#include "Pancake.h"
+#include "Pancake_stack.h"
+
 Game_State state = Game_State::Start;
+Pancake_stack pp;
 
 Game_State Screen2::get_state()
 {
@@ -12,40 +16,87 @@ Game_State Screen2::get_state()
 Screen2::Screen2(Point xy, int w, int h, const string& title)
         : Window(xy, w, h, title),
         to_game_state(Point(x_max() - 200, 0), 70, 20, "TO GAME", cb_to_game_state),
-        to_end_state(Point(x_max() - 120, 0), 70, 20, "TO END", cb_to_end_state)
+        to_end_state(Point(x_max() - 120, 0), 70, 20, "TO END", cb_to_end_state),
+        to_quit(Point(x_max() - 120, 60), 70, 20, "QUIT", cb_quit)
 
 {
         attach(to_game_state);
         attach(to_end_state);
+        attach(to_quit);
+
         to_end_state.hide();
-//      Rectangle * r = new Rectangle(Point(10,10),10,10);
-//      attach(*r);
+        to_quit.hide();
 }
 
 void Screen2::change(Game_State s)
 {
-//      r_buffer.push_back(new Rectangle(Point(50, 60), 10, 10));
-        //attach((Rectangle) r_buffer[0]);
-//      attach( *(Rectangle*) (&r_buffer[0]));
-
-///     Rectangle * r = new Rectangle(Point(20,20),20,20);
-///     attach(*r);
-//      cout << "try attach" << endl;
-///     Fl::redraw();
-
-        to_game_state.hide();
-        to_end_state.show();
+//      to_game_state.hide();
+//      to_end_state.show();
         state = s;
 }
 
 void Screen2::game_state_pressed()
 {
-        Rectangle * r = new Rectangle(Point(20, 20), 20, 20);
-        attach(*r);
+//      Rectangle * r = new Rectangle(Point(20, 20), 20, 20);
+//      attach(*r);
+
+        pp.init_stack();
+
+        for(int i = 0; i < pp.pancakes.size(); ++i)
+        {
+                attach(pp.pancakes[i]);
+
+        }
+
+        to_game_state.hide();
+        to_quit.hide();
+        to_end_state.show();
+
+        Fl::redraw();
+
+        change(Game_State::Game);
+}
+
+void Screen2::end_state_pressed()
+{
+        for(int i = 0; i < pp.pancakes.size(); ++i)
+        {
+                detach(pp.pancakes[i]);
+        }
+
+        to_end_state.hide();
+        to_quit.show();
+        to_game_state.show();
+
+        Fl::redraw();
+
+        change(Game_State::Game);
+}
+
+void Screen2::end_state_pressed()
+{
+        for(int i = 0; i < pp.pancakes.size(); ++i)
+        {
+                detach(pp.pancakes[i]);
+        }
+
+        to_end_state.hide();
+        to_quit.show();
+        to_game_state.show();
 
         Fl::redraw();
 
         change(Game_State::End);
+}
+
+void Screen2::quit()
+{
+        hide();
+}
+
+void Screen2::cb_quit(Address, Address pw)
+{
+        reference_to<Screen2>(pw).quit();
 }
 
 void Screen2::cb_to_game_state(Address, Address pw)
