789eff5980c59c64963d3f13b56bf14ad605b40c
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-13 13:31:06 -0500

    Update Screen2.h

--- Screen2.h
+++ Screen2.h
@@ -12,6 +12,9 @@
 using namespace Graph_lib;
 using namespace std;
 
+class Pancake;
+class Pancake_stack;
+
 enum Game_State
 {
         Start, Game, End
@@ -27,13 +30,16 @@ struct Screen2 : Graph_lib::Window
 
                 void game_state_pressed();
                 void end_state_pressed();
+                void quit();
 
                 static void cb_to_game_state(Address, Address);
                 static void cb_to_end_state(Address, Address);
+                static void cb_quit(Address, Address);
         private:
-               Button to_game_state;
-               Button to_end_state;
-               Vector_ref<Shape> r_buffer;
+                Button to_game_state;
+                Button to_end_state;
+                Button to_quit;
+                Vector_ref<Shape> r_buffer;
 //               In_box difficulty;
 };
 
