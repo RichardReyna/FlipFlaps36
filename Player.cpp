49827544157d331abf21c0c7379d1d833fb13e4c
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-17 12:44:05 -0500

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,26 @@
+#include "Player.h"
+
+int Player::get_score()
+{
+        return score;
+}
+
+void Player::set_name(string s)
+{
+        name = s;
+}
+
+void Player::set_score(int n)
+{
+        score = n;
+}
+
+void Player::store_name()
+{
+        //...
+}
+
+void Player::store_score()
+{
+        //...
+}
