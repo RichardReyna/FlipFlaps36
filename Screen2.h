1a45a501ca75b562a502a97374daee17107f4e1e
RoniloRagodos <RoniloRagodos57@hotmail.com>
2014-04-17 12:38:34 -0500

    Update Screen2.h

--- Screen2.h
+++ Screen2.h
@@ -25,22 +25,45 @@ struct Screen2 : Graph_lib::Window
         public:
                 Screen2(Point, int, int, const string&);
                 Game_State get_state();
+                int get_diff();
         private:
                 void change(Game_State s);
 
                 void game_state_pressed();
+//              int get_diff();
                 void end_state_pressed();
                 void quit();
+                void set_place(int);
 
                 static void cb_to_game_state(Address, Address);
                 static void cb_to_end_state(Address, Address);
+                static void cb_get_diff(Address, Address);
                 static void cb_quit(Address, Address);
+                static void cb_place_1(Address, Address);
+/*                static void cb_place_2(Address, Address);
+                static void cb_place_3(Address, Address);
+                static void cb_place_4(Address, Address);
+                static void cb_place_5(Address, Address);
+                static void cb_place_6(Address, Address);
+                static void cb_place_7(Address, Address);
+                static void cb_place_8(Address, Address);
+                static void cb_place_9(Address, Address);*/
         private:
                 Button to_game_state;
                 Button to_end_state;
                 Button to_quit;
-                Vector_ref<Shape> r_buffer;
-//               In_box difficulty;
+                In_box difficulty;
+                Button place_1;
+/*                Button place_2;
+                Button place_3;
+                Button place_4;
+                Button place_5;
+                Button place_6;
+                Button place_7;
+                Button place_8;
+                Button place_9;*/
+                int diff;
+                int place;
 };
 
 #endif
